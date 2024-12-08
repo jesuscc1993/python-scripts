001ac09b0a6419bb949a5a958760f816a4f55dd3
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2024-12-08 20:17:06 +0100

    Extracted const.

--- generate-file-associations/generate-notepad-file-associations.py
+++ generate-file-associations/generate-notepad-file-associations.py
@@ -4,6 +4,7 @@ import winreg as reg
 EXE_PATH = r'Y:\Software\Development\Notepad++\notepad++.exe'
 ICONS_PATH = r'Y:\Images\Icons\Packs\File Types\ICO'
 REG_ROOT = r'SOFTWARE\Classes'
+FILE_EXTS = r'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts'
 EXTENSIONS = ['cfg', 'css', 'csv', 'gitignore', 'ini', 'js', 'json', 'less', 'lua', 'md', 'scss', 'srt', 'ts', 'txt', 'xml']
 
 def delete_registry_entry(path):
@@ -24,13 +25,13 @@ def add_registry_entry(path, name, value):
 def main():
   for ext in EXTENSIONS:
     icon_path = os.path.join(ICONS_PATH, f'{ext}.ico')
-    file_type = f'{ext.upper()}File'
+    file_type = f'{ext.lower()}file'
 
     delete_registry_entry(f'.{ext}\\OpenWithProgids')
     add_registry_entry(f'.{ext}', '', file_type)
     add_registry_entry(f'{file_type}\\shell\\open\\command', '', f'"{EXE_PATH}" "%1"')
     add_registry_entry(f'{file_type}\\DefaultIcon', '', icon_path)
-    add_registry_entry(f'HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts\\.{ext}\\UserChoice', 'Progid', file_type)
+    add_registry_entry(f'{FILE_EXTS}\\.{ext}\\UserChoice', 'Progid', file_type)
 
   print('Registry entries created successfully!')
 
