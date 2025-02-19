6d70d7ee098314de9e87f4a5ae3d5d5b0027394d
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-02-19 21:36:48 +0100

    Excluded folder images.

--- manga-resizer/_image_utils.py
+++ manga-resizer/_image_utils.py
@@ -1,10 +1,13 @@
 JPEG_EXTENSION = '.jpeg'
 JPG_EXTENSION = '.jpg'
 PNG_EXTENSION = '.png'
-WEB_EXTENSION = '.webp'
+WEBP_EXTENSION = '.webp'
+
+FILE_EXCLUSIONS = ['folder.jpg', 'cover.jpg']
 
 def is_image_file(filename):
-  return filename.lower().endswith((JPG_EXTENSION, JPEG_EXTENSION, PNG_EXTENSION, WEB_EXTENSION))
+  name = filename.lower()
+  return name.endswith((JPG_EXTENSION, JPEG_EXTENSION, PNG_EXTENSION, WEBP_EXTENSION)) and name not in FILE_EXCLUSIONS
 
 def is_image_uncompressed(filename):
   return filename.lower().endswith((PNG_EXTENSION))
