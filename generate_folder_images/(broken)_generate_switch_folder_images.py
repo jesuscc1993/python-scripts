62e5b966121ec7844e81e06941360fe31b634970
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-04-18 20:21:40 +0200

    Added (broken)_generate_switch_folder_images

---
+++ generate_folder_images/(broken)_generate_switch_folder_images.py
@@ -0,0 +1,45 @@
+import os
+import re
+import requests
+import webbrowser
+
+from PIL import Image
+from io import BytesIO
+
+from _common import process_parent_folder, save_resized_image
+
+IMAGE_URL = "https://tinfoil.media/ti/{game_id}/256/256/"
+
+def main():
+  process_parent_folder(process_folder)
+
+def process_folder(folder_path):
+  folder_name = os.path.basename(os.path.normpath(folder_path))
+  match = re.match(r'(^[0-9A-F]+$)', folder_name.upper())
+
+  if match:
+    game_id = match.group(1)
+  else:
+    print(f'Could not extract a valid game ID from "{folder_name}"')
+    return
+
+  download_game_cover(game_id, folder_path)
+
+def download_game_cover(game_id, folder_path):
+  image_url = IMAGE_URL.format(game_id = game_id)
+  # webbrowser.open(image_url)
+  response = requests.get(image_url)
+
+  if response.status_code != 200:
+    print(f'Failed to access the search results for {game_id}. Status code: {response.status_code}')
+    return
+
+  img = Image.open(BytesIO(response.content))
+  save_resized_image(img, folder_path)
+
+if __name__ == '__main__':
+  try:
+    main()
+  except Exception as e:
+    print(f'An unexpected error occurred: {e}')
+  input('Press Enter to exit...')
