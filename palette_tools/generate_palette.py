8de1cc406120834346968edc1fc6a8ea4a13762a
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-04-23 23:59:57 +0200

    Added generate_palette.py

---
+++ palette_tools/generate_palette.py
@@ -0,0 +1,59 @@
+import colorsys
+import os
+
+from PIL import Image, ImageDraw
+from collections import Counter
+
+SQUARE_SIZE = 40
+COLUMNS = 8
+
+def main():
+  src_path = input('Enter the path to the image you want to generate the palette for:\n').strip('" ')
+  generate_palette(src_path)
+
+def generate_palette(image_path):
+  colors = extract_colors(image_path)
+  folder, filename = os.path.split(image_path)
+  name, _ = os.path.splitext(filename)
+  output_name = f'{name} (palette).png'
+  output_path = os.path.join(folder, output_name)
+  create_palette_image(colors, output_path)
+
+def extract_colors(image_path):
+  img = Image.open(image_path).convert('RGBA')
+  pixels = [p for p in img.getdata() if p[3] > 0]
+  count = Counter(pixels)
+  def sort_key(c):
+    r, g, b = [v / 255 for v in c[0][:3]]
+    h, l, s = colorsys.rgb_to_hls(r, g, b)
+    return (h, s, l)
+  sorted_colors = sorted(count.items(), key = lambda x: sort_key(x))
+  return [c[0] for c in sorted_colors]
+
+def create_palette_image(colors, output_path):
+  rows = (len(colors) + COLUMNS - 1) // COLUMNS
+  width = COLUMNS * SQUARE_SIZE
+  height = rows * SQUARE_SIZE
+  palette_img = Image.new('P', (width, height))
+  palette = []
+
+  for color in colors:
+    palette.extend(color[:3])
+  palette += [0] * (768 - len(palette))
+  palette_img.putpalette(palette)
+  draw = ImageDraw.Draw(palette_img)
+
+  for i, color in enumerate(colors):
+    x = (i % COLUMNS) * SQUARE_SIZE
+    y = (i // COLUMNS) * SQUARE_SIZE
+    draw.rectangle([x, y, x + SQUARE_SIZE, y + SQUARE_SIZE], fill=colors.index(color))
+
+  palette_img.save(output_path, format = 'PNG', save_all=False)
+  print(f'Saved "{output_path}".')
+
+if __name__ == '__main__':
+  try:
+    main()
+  except Exception as e:
+    print(f'An unexpected error occurred: {e}')
+  input('Press Enter to exit...')
