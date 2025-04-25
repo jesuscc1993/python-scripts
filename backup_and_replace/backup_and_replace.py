dcc6ea8bac3d3b7e706a46386c03a77f93f9b157
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-04-25 15:19:59 +0200

    Added backup_and_replace.py

---
+++ backup_and_replace/backup_and_replace.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+
+def main():
+  src_path = prompt_path('Enter the path containing the replacement files:\n')
+  if src_path is None: return
+  print('')
+
+  dest_path = prompt_path('Enter the path the files will be copied to:\n')
+  if dest_path is None: return
+  print('')
+
+  rename_and_copy_files(src_path, dest_path)
+  print('')
+
+  print(f'Finished copying "{src_path}" to "{dest_path}".\n')
+  main()
+
+def prompt_path(prompt_message):
+  path = input(prompt_message).strip('" ')
+  if not path or not os.path.isdir(path):
+    print(f'The specified path "{path}" is not a directory.')
+    return None
+  return path
+
+def rename_and_copy_files(src_path, dest_path):
+  for filename in os.listdir(src_path):
+    src_file = os.path.join(src_path, filename)
+    if os.path.isfile(src_file):
+      dest_file = os.path.join(dest_path, filename)
+      backup_file(dest_file)
+      shutil.copy(src_file, dest_file)
+
+def backup_file(file_path):
+  if os.path.isfile(file_path):
+    name, ext = os.path.splitext(file_path)
+    new_filename = f'{name}.bak{ext}'
+    bak_path = os.path.join(os.path.dirname(file_path), new_filename)
+
+    if not os.path.isfile(bak_path):
+      os.rename(file_path, bak_path)
+      print(f'Saved backup file "{bak_path}".')
+    else:
+      print(f'Backup file "{bak_path}" already exists and will be reused.')
+
+if __name__ == '__main__':
+  try:
+    main()
+  except Exception as e:
+    print(f'An unexpected error occurred: {e}')
+    input('Press Enter to exit...')
