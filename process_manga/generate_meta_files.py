c3a7488e077c424e2b86fa30bf380eed8463fb20
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-05-02 23:03:33 +0200

    Added half-assed generate_meta_files.py

---
+++ process_manga/generate_meta_files.py
@@ -0,0 +1,47 @@
+import os
+
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+
+META_FILES = ['.noxml', '.nomedia']
+
+def main():
+  parent_folder = input('Enter the path to the image you want to generate the palette for:\n')
+  if not parent_folder:
+    return
+  if not os.path.isdir(parent_folder):
+    print(f'The specified path "{parent_folder}" is not a directory.')
+  else:
+    process_parent_folder(parent_folder)
+
+def process_parent_folder(parent_folder):
+  folders_to_process = []
+
+  with os.scandir(parent_folder) as entries:
+    for entry in entries:
+      if entry.is_dir():
+        folders_to_process.append(entry.path)
+
+  with ThreadPoolExecutor(max_workers=1) as executor:
+    executor.map(process_folder, folders_to_process)
+
+def process_folder(folder_path):
+  for filename in META_FILES:
+    try:
+      file_path = os.path.join(folder_path, filename)
+      if os.path.exists(file_path):
+        print(f'File "{file_path}" already exists. Skipping...')
+      else:
+        open(file_path, 'w').close()
+        os.system(f'attrib +h "{file_path}"')
+        print(f'Successfully created hidden file "{file_path}".')
+
+    except Exception as e:
+      print(f'ERROR: Failed to create "{filename}": {e}')
+
+if __name__ == '__main__':
+  try:
+    main()
+  except Exception as e:
+    print(f'An unexpected error occurred: {e}')
+  input('Press Enter to exit...')
