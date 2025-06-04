392b85ac8003434ee4a192aaa526c5f07216024f
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-06-04 12:44:46 +0200

    Added filters

--- steam/generate_steam_app_ids.py
+++ steam/generate_steam_app_ids.py
@@ -7,11 +7,30 @@ from _env import steam_api_key, steam_user_id
 OUTPUT_PATH = './output'
 OUTPUT_FILE = os.path.join(OUTPUT_PATH, 'app-ids.json')
 
-url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={steam_api_key}&steamid={steam_user_id}&include_appinfo=true'
-response = requests.get(url).json()
-games = response.get('response', {}).get('games', [])
-app_ids = sorted([game['appid'] for game in games])
+# filters
+PLAYED = None
+WITH_STATS = None
 
-os.makedirs(OUTPUT_PATH, exist_ok = True)
-with open(OUTPUT_FILE, 'w') as f:
-  json.dump(app_ids, f, indent = 2)
+def main():
+  url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={steam_api_key}&steamid={steam_user_id}&include_appinfo=true'
+  response = requests.get(url).json()
+  games = response.get('response', {}).get('games', [])
+
+  filtered_app_ids = sorted(game['appid'] for game in games if filter_game(game))
+
+  os.makedirs(OUTPUT_PATH, exist_ok = True)
+  with open(OUTPUT_FILE, 'w') as f:
+    json.dump(filtered_app_ids, f, indent = 2)
+
+def filter_game(game):
+  if PLAYED is not None:
+    played = game.get('playtime_forever', 0) > 0
+    if played != PLAYED:
+      return False
+  if WITH_STATS is not None:
+    has_stats = bool(game.get('has_community_visible_stats'))
+    if has_stats != WITH_STATS:
+      return False
+  return True
+
+main()
