435e46be2a9836fece7b5232244a1aa9aa80be74
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-07-05 16:20:22 +0200

    Minor tweaks.

--- copy_files/backup_files.py
+++ copy_files/backup_files.py
@@ -12,13 +12,13 @@ def main():
     src_path = prompt_path('Enter the path containing the files to copy:\n')
     dest_path = prompt_path('Enter the path the files will be copied to:\n')
 
-  copy_updated_files(src_path, dest_path)
+  backup_updated_files(src_path, dest_path)
 
-def copy_updated_files(src, dest):
-  for src_dir, _, files in os.walk(src):
-    relative_path = os.path.relpath(src_dir, src)
-    dest_dir = os.path.join(dest, relative_path)
-    os.makedirs(dest_dir, exist_ok=True)
+def backup_updated_files(src_path, dest_path):
+  for src_dir, _, files in os.walk(src_path):
+    relative_path = os.path.relpath(src_dir, src_path)
+    dest_dir = os.path.join(dest_path, relative_path)
+    os.makedirs(dest_dir, exist_ok = True)
 
     for file in files:
       src_file = os.path.join(src_dir, file)
--- copy_files/restore_backups.py
+++ copy_files/restore_backups.py
@@ -1,22 +1,23 @@
 import os
+import shutil
 
 from _common import prompt_path
 
 def main():
   dir_path = prompt_path('Enter the directory containing .bak files to restore:\n')
 
-  restore_bak_files(dir_path)
+  restore_backups(dir_path)
   print(f'\nFinished restoring .bak files in "{dir_path}".\n')
   main()
 
-def restore_bak_files(dir_path):
+def restore_backups(dir_path):
 	for filename in os.listdir(dir_path):
 		if '.bak.' in filename:
 			bak_path = os.path.join(dir_path, filename)
 			og_name = filename.replace('.bak.', '.', 1)
 			og_path = os.path.join(dir_path, og_name)
 
-			os.replace(bak_path, og_path)
+			shutil.copy2(bak_path, og_path)
 			print(f'Restored "{bak_path}" as "{og_path}".')
 
 if __name__ == '__main__':
