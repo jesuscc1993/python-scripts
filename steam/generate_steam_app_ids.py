e9fc7e4ea1381084f5a726e831433484b0df1143
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-07-14 16:21:05 +0200

    Minor refactor.

--- steam/generate_steam_app_ids.py
+++ steam/generate_steam_app_ids.py
@@ -12,15 +12,16 @@ PLAYED = None
 WITH_STATS = None
 
 def main():
-  url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={steam_api_key}&steamid={steam_user_id}&include_appinfo=true'
-  response = requests.get(url).json()
-  games = response.get('response', {}).get('games', [])
-
+  games = get_owned_games(steam_api_key, steam_user_id)
   filtered_app_ids = sorted(game['appid'] for game in games if filter_game(game))
 
   os.makedirs(OUTPUT_PATH, exist_ok = True)
   with open(OUTPUT_FILE, 'w') as f:
     json.dump(filtered_app_ids, f, indent = 2)
+
+def get_owned_games(api_key, steam_id):
+  url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={api_key}&steamid={steam_id}&include_appinfo=true'
+  return requests.get(url).json().get('response', {}).get('games', [])
 
 def filter_game(game):
   if PLAYED is not None:
