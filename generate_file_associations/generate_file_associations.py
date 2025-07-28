deb734d9c741b3a823ef5a491d1b07c7de8e2d2f
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-07-28 16:43:01 +0200

    Fixed UserChoice path

--- generate_file_associations/generate_file_associations.py
+++ generate_file_associations/generate_file_associations.py
@@ -4,7 +4,14 @@ import os
 import winreg
 
 ASSOCIATIONS_JSON = './associations.json'
-FILE_EXTS = 'HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts'
+FILE_EXTS = 'Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts'
+HKEY_NAMES = {
+  winreg.HKEY_CLASSES_ROOT: 'HKEY_CLASSES_ROOT',
+  winreg.HKEY_CURRENT_CONFIG: 'HKEY_CURRENT_CONFIG',
+  winreg.HKEY_CURRENT_USER: 'HKEY_CURRENT_USER',
+  winreg.HKEY_LOCAL_MACHINE: 'HKEY_LOCAL_MACHINE',
+  winreg.HKEY_USERS: 'HKEY_USERS',
+}
 
 def main():
   if ctypes.windll.shell32.IsUserAnAdmin() == 0:
@@ -30,17 +37,17 @@ def main():
 
       ext_icon_path = get_icon_path(icons_path, ext_icon.upper())
       fallback_icon_path = get_icon_path(icons_path, fallback_icon)
-      file_type = get_registry_value(f'.{ext}', '') or f'{ext.lower()}file'
+      file_type = get_registry_value(winreg.HKEY_CLASSES_ROOT, f'.{ext}', '') or f'{ext.lower()}file'
 
-      delete_registry_entry(f'{FILE_EXTS}\\.{ext}\\UserChoice')
-      add_registry_entry(f'.{ext}', '', file_type)
+      delete_registry_entry(winreg.HKEY_CURRENT_USER, f'{FILE_EXTS}\\.{ext}\\UserChoice')
+      add_registry_entry(winreg.HKEY_CLASSES_ROOT, f'.{ext}', '', file_type)
 
       if type_name:
-        add_registry_entry(f'{file_type}', 'FriendlyTypeName', type_name)
+        add_registry_entry(winreg.HKEY_CLASSES_ROOT, f'{file_type}', 'FriendlyTypeName', type_name)
 
       type_icon = ext_icon_path or fallback_icon_path
       if type_icon and os.path.exists(type_icon):
-        add_registry_entry(f'{file_type}\\DefaultIcon', '', f'"{type_icon}"')
+        add_registry_entry(winreg.HKEY_CLASSES_ROOT, f'{file_type}\\DefaultIcon', '', f'"{type_icon}"')
 
       if shell:
         for item in shell:
@@ -49,14 +56,14 @@ def main():
           key = item.get('key')
           label = item.get('label')
 
-          add_registry_entry(f'{file_type}\\shell\\{key}', '', label or '')
+          add_registry_entry(winreg.HKEY_CLASSES_ROOT, f'{file_type}\\shell\\{key}', '', label or '')
 
           if icon:
-            add_registry_entry(f'{file_type}\\shell\\{key}', 'Icon', icon)
+            add_registry_entry(winreg.HKEY_CLASSES_ROOT, f'{file_type}\\shell\\{key}', 'Icon', icon)
           else:
-            delete_registry_entry(f'{file_type}\\shell\\{key}', 'Icon')
+            delete_registry_entry(winreg.HKEY_CLASSES_ROOT, f'{file_type}\\shell\\{key}', 'Icon')
 
-          add_registry_entry(f'{file_type}\\shell\\{key}\\command', '', command)
+          add_registry_entry(winreg.HKEY_CLASSES_ROOT, f'{file_type}\\shell\\{key}\\command', '', command)
 
       print(f'[LOG] Saved registry key "HKEY_CLASSES_ROOT\\{file_type}" for extension "HKEY_CLASSES_ROOT\\.{ext}".\n')
 
@@ -66,39 +73,42 @@ def get_associations():
   with open(ASSOCIATIONS_JSON, 'r') as associations:
     return json.load(associations)
 
-def get_registry_value(path, name):
+def get_registry_value(root, path, name):
   try:
-    with winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path) as key:
+    with winreg.OpenKey(root, path) as key:
       return winreg.QueryValueEx(key, name)[0]
   except FileNotFoundError:
     return None
   except Exception as ex:
-    print(f'[ERROR] Could not get value for registry entry "{path}": {ex}')
+    print(f'[ERROR] Could not get value for registry entry "{get_registry_key(root, path, name)}": {ex}')
     return None
 
-def delete_registry_entry(path, name = None):
+def delete_registry_entry(root, path, name = None):
   try:
     if not name:
-      winreg.DeleteKey(winreg.HKEY_CLASSES_ROOT, path)
-      print(f'Deleted key: "HKEY_CLASSES_ROOT\\{path}"')
+      winreg.DeleteKey(root, path)
+      print(f'Deleted key: "{get_registry_key(root, path)}"')
     else:
-      with winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path, 0, winreg.KEY_SET_VALUE) as key:
+      with winreg.OpenKey(root, path, 0, winreg.KEY_SET_VALUE) as key:
         winreg.DeleteValue(key, name)
-        print(f'Deleted value: "HKEY_CLASSES_ROOT\\{path}\\{name}"')
-  except FileNotFoundError as ex:
-    full_path = f'HKEY_CLASSES_ROOT\\{path}\\{name}' if name else f'HKEY_CLASSES_ROOT\\{path}'
-    print(f'[WARN] Could not find registry entry {full_path}')
-    pass
+        print(f'Deleted value: "{get_registry_key(root, path, name)}"')
+  except FileNotFoundError:
+    print(f'[WARN] Could not find registry entry "{get_registry_key(root, path, name)}"')
   except Exception as ex:
-    print(f'[ERROR] Could not delete registry entry "{path}": {ex}')
+    print(f'[ERROR] Could not delete registry entry "{get_registry_key(root, path)}": {ex}')
 
-def add_registry_entry(path, name, value):
+def add_registry_entry(root, path, name, value):
   try:
-    with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, path) as key:
+    with winreg.CreateKey(root, path) as key:
       winreg.SetValueEx(key, name, 0, winreg.REG_SZ, value)
-      print(f'[INFO] Set registry entry "HKEY_CLASSES_ROOT\\{path}\\{name}" with value "{value}"')
+      print(f'[INFO] Set registry entry "{get_registry_key(root, path, name)}" with value "{value}"')
   except Exception as ex:
-    print(f'[ERROR] Could not add registry entry for "{path}": {ex}')
+    print(f'[ERROR] Could not add registry entry for "{get_registry_key(root, path)}": {ex}')
+
+
+def get_registry_key(root, path, name = None):
+  root_name = HKEY_NAMES.get(root, str(root))
+  return f'{root_name}\\{path}' + (f'\\{name}' if name else '')
 
 def get_icon_path(icons_path, name):
   path = os.path.join(icons_path, f'{name}.ico')
