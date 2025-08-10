36680f27d5d8e3268910c5cc416a1a94fc201033
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-08-10 14:25:06 +0200

    Added override_sagethumbs.py

---
+++ generate_file_associations/override_sagethumbs.py
@@ -0,0 +1,37 @@
+import winreg
+
+IMAGE_EXTENSIONS = [
+  'bmp',
+  'dds',
+  'gif',
+  'ico',
+  'jpeg',
+  'jpg',
+  'png',
+  'svg',
+  'tga',
+  'tif',
+  'tiff',
+  'webp'
+]
+
+def main():
+  for ext in IMAGE_EXTENSIONS:
+    update_key(f'SageThumbsImage.{ext}')
+    # update generic class too in case sagethumbs is nto set up for this particular extension
+    update_key(f'{ext}file')
+
+def update_key(key_name):
+  try:
+    with winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, key_name, 0, winreg.KEY_SET_VALUE) as subkey:
+      winreg.SetValueEx(subkey, '', 0, winreg.REG_SZ, 'Image File')
+      print(f'[DEBUG] Updated "{key_name}".')
+  except FileNotFoundError:
+    pass
+
+if __name__ == '__main__':
+  try:
+    main()
+  except Exception as ex:
+    print(f'An unexpected error occurred: {ex}')
+  input('\nPress Enter to exit...')
