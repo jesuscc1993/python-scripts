285253f7ccc50807e86c9a6b18aa05d006236251
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-10-29 20:14:45 +0100

    Added unused function because saving WEBP as mono barely reduces file size.

--- compress/_image_utils.py
+++ compress/_image_utils.py
@@ -1,3 +1,7 @@
+import numpy
+
+from PIL import Image
+
 JPEG_EXTENSION = '.jpeg'
 JPG_EXTENSION = '.jpg'
 PNG_EXTENSION = '.png'
@@ -9,3 +13,10 @@ WEBP_DIMENSION_LIMIT = 16383
 def is_image_file(filename):
   name = filename.lower()
   return name.endswith((JPG_EXTENSION, JPEG_EXTENSION, PNG_EXTENSION, WEBP_EXTENSION)) and name not in FILE_EXCLUSIONS
+
+def is_image_monochrome(img, sample_step = 24, sat_threshold = 24):
+  hsv = img.convert('HSV')
+  arr = numpy.array(hsv)
+  arr = arr[::sample_step, ::sample_step]
+  s = arr[..., 1]
+  return numpy.all(s < sat_threshold)
