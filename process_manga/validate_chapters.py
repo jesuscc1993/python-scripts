4f679f36440807b6f85bd7d8e7dc43e8f9bca918
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-12-07 11:35:31 +0100

    Refactored script to also check for missing pages.

--- process_manga/validate_chapters.py
+++ process_manga/validate_chapters.py
@@ -17,6 +17,7 @@ def main():
     )
 
 def process_parent_folder(directory):
+  directory_name = os.path.basename(directory)
   pattern = re.compile(r'(?:Ch(?:apter)?|Ep(?:isode)?)\.?\s*(\d+)', re.IGNORECASE)
   found_chapters = set()
 
@@ -25,19 +26,45 @@ def process_parent_folder(directory):
     if match:
       found_chapters.add(int(match.group(1)))
 
+    if entry.is_dir():
+      process_chapter_folder(entry.path)
+
   if not found_chapters:
-    logger.log('No chapters found in "{directory}".')
+    logger.log(f'No chapters found in "{directory_name}".')
     return
 
   expected_chapters = set(range(1, max(found_chapters) + 1))
   missing_chapters = expected_chapters - found_chapters
   if missing_chapters:
     logger.log(
-      f'\nChapters missing in "{directory}": '
+      f'\nChapters missing in "{directory_name}": '
       f'{" ".join(f"{ch:03d}" for ch in sorted(missing_chapters))}'
     )
   else:
     logger.log(f'\nNo chapters found missing in "{directory}".')
+
+def process_chapter_folder(directory):
+  directory_name = os.path.basename(directory)
+  pattern = re.compile(r'(\d+)', re.IGNORECASE)
+  found_pages = set()
+
+  for entry in os.scandir(directory):
+    if entry.is_file():
+      match = pattern.search(entry.name)
+      if match:
+        found_pages.add(int(match.group(1)))
+
+  if not found_pages:
+    logger.log(f'No pages found in "{directory_name}".')
+    return
+
+  expected_pages = set(range(1, max(found_pages) + 1))
+  missing_pages = expected_pages - found_pages
+  if missing_pages:
+    logger.log(
+      f'Pages missing in "{directory_name}": '
+      f'{" ".join(f"{p:02d}" for p in sorted(missing_pages))}'
+    )
 
 if __name__ == '__main__':
   try:
