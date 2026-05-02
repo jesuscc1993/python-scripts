37a195eb3b4576c64dafe7ed9b9511eef66b79ff
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-05-02 19:30:24 +0200

    Added shortcut script to compress.

---
+++ process_manga/compress_folders.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+from mtlogger import logger
+
+from _common import exit_with_prompt, print_error, select_parent_folder, run_scripts_in_sequence
+
+SCRIPT_NAMES = [
+  '../compress/compress_folders'
+]
+
+def process_parent_folder(parent_folder):
+  run_scripts_in_sequence(SCRIPT_NAMES, parent_folder)
+
+if __name__ == '__main__':
+  try:
+    select_parent_folder('Enter the path to the parent folder you want to process:\n', process_parent_folder)
+  except Exception as ex:
+    print_error(ex)
+    exit_with_prompt()
