38737fa65ae2b39459834e82c6c60fc05c054808
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-05-18 17:26:21 +0200

    Fixed encoding.

--- process_video/_common.py
+++ process_video/_common.py
@@ -5,17 +5,17 @@ import sys
 from mtlogger import logger
 
 def add_missing_spaces(file_path):
-  with open(file_path, 'r', encoding = 'utf-8', errors = 'replace') as f:
+  with open(file_path, 'r', encoding = 'latin-1', errors = 'replace') as f:
     content = f.read()
   content = re.sub(r'(?<=[a-záéíóúüñ])([.,;:!?]+)([A-ZÁÉÍÓÚÜÑ])', r'\1 \2', content)
-  with open(file_path, 'w', encoding = 'utf-8') as f:
+  with open(file_path, 'w', encoding = 'latin-1') as f:
     f.write(content)
 
 def strip_tags_from_subs_file(file_path):
-  with open(file_path, 'r', encoding = 'utf-8', errors = 'replace') as f:
+  with open(file_path, 'r', encoding = 'latin-1', errors = 'replace') as f:
     content = f.read()
   content = re.sub(r'</?font\b[^>]*>', '', content, flags = re.IGNORECASE)
-  with open(file_path, 'w', encoding = 'utf-8') as f:
+  with open(file_path, 'w', encoding = 'latin-1') as f:
     f.write(content)
 
 def prompt_path(prompt_message, optional = False):
