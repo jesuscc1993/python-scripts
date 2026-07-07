18e275b3482f5249389a358fad8683f3329b946b
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-07-07 23:09:06 +0200

    Extract asset download logic to common

---
+++ steam/_common.py
@@ -0,0 +1,62 @@
+import os
+import requests
+
+from PIL import Image
+from dotenv import load_dotenv
+from io import BytesIO
+from mtlogger import logger
+from pathlib import Path
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+COVER_H = os.environ.get('COVER_H')
+COVER_W = os.environ.get('COVER_W')
+HEADER_H = os.environ.get('HEADER_H')
+HEADER_W = os.environ.get('HEADER_W')
+
+COVER_H = int(COVER_H) if COVER_H else None
+COVER_W = int(COVER_W) if COVER_W else None
+HEADER_H = int(HEADER_H) if HEADER_H else None
+HEADER_W = int(HEADER_W) if HEADER_W else None
+
+COVER_URL_MAP = {
+  'header': {
+    'url': 'https://steamcdn-a.akamaihd.net/steam/apps/{}/header.jpg',
+    'dest': '{}.jpg',
+    'size': [HEADER_W, HEADER_H] if HEADER_W and HEADER_H else None
+  },
+  'library': {
+    'url': 'https://steamcdn-a.akamaihd.net/steam/apps/{}/library_600x900_2x.jpg',
+    'dest': '{}p.jpg',
+    'size': [COVER_W, COVER_H] if COVER_W and COVER_H else None
+  }
+}
+
+HEADER_SIZE = COVER_URL_MAP['header']['size']
+COVER_SIZE = COVER_URL_MAP['library']['size']
+
+def download_assets_for_app_id(steam_app_id, dest_dir, filename_id = None):
+  filename_id = filename_id or steam_app_id
+  Path(dest_dir).mkdir(parents = True, exist_ok = True)
+  for _, data in COVER_URL_MAP.items():
+    url = data['url'].format(steam_app_id)
+    filename = data['dest'].format(filename_id)
+    download_asset_for_app_id(url, Path(dest_dir) / filename, data.get('size'))
+
+def download_asset_for_app_id(url, dest, size = None):
+  if Path(dest).exists():
+    logger.trace(f'Skipping {Path(dest).name}: already exists')
+    return
+  response = requests.get(url)
+  response.raise_for_status()
+  save_asset(response.content, dest, size)
+
+def save_asset(content, filepath, size = None):
+  try:
+    img = Image.open(BytesIO(content))
+    if size:
+      img = img.resize(size, Image.LANCZOS)
+    img.save(filepath)
+    logger.success(f'Saved "{filepath}"')
+  except Exception as ex:
+    logger.error(f'Could not save {filepath}:\n{ex}')
--- steam/download_game_assets.py
+++ steam/download_game_assets.py
@@ -1,23 +1,10 @@
 import os
 import requests
 
-from dotenv import load_dotenv
-from PIL import Image
-from io import BytesIO
 from mtlogger import logger
 from mtprompt import Prompt
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-
-COVER_H = os.environ.get('COVER_H')
-COVER_W = os.environ.get('COVER_W')
-HEADER_H = os.environ.get('HEADER_H')
-HEADER_W = os.environ.get('HEADER_W')
-
-COVER_H = int(COVER_H) if COVER_H else None
-COVER_W = int(COVER_W) if COVER_W else None
-HEADER_H = int(HEADER_H) if HEADER_H else None
-HEADER_W = int(HEADER_W) if HEADER_W else None
+from _common import download_assets_for_app_id
 
 # settings
 TERMS_BLACKLIST = ['soundtrack', 'artbook']
@@ -26,19 +13,6 @@ OUTPUT_FOLDER = os.path.join('output', 'assets')
 SEARCH_URL = 'https://store.steampowered.com/api/storesearch/'
 SEARCH_PARAMS = {'term': '', 'l': 'english', 'cc': 'US'}
 RESPONSE_NAME = 'name'
-
-COVER_URL_MAP = {
-  'header': {
-    'url': 'https://steamcdn-a.akamaihd.net/steam/apps/{}/header.jpg',
-    'dest': '{}.jpg',
-    'size': [HEADER_W, HEADER_H] if HEADER_W and HEADER_H else None
-  },
-  'library': {
-    'url': 'https://steamcdn-a.akamaihd.net/steam/apps/{}/library_600x900.jpg',
-    'dest': '{}p.jpg',
-    'size': [COVER_W, COVER_H] if COVER_W and COVER_H else None
-  }
-}
 
 session = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0'}
@@ -72,7 +46,7 @@ def main():
         continue
 
       selected = items[choice - 1]
-      download_game_assets(selected.get('id'))
+      download_assets_for_app_id(selected.get('id'), os.path.join(os.getcwd(), OUTPUT_FOLDER))
       break
 
 def search_game(name):
@@ -87,33 +61,6 @@ def search_game(name):
     logger.error(f'Error searching for game "{name}":\n{ex}')
     return []
 
-def download_game_assets(appid):
-  output_dir = os.path.join(os.getcwd(), OUTPUT_FOLDER)
-  os.makedirs(output_dir, exist_ok = True)
-
-  for _, data in COVER_URL_MAP.items():
-    url = data['url'].format(appid)
-    filename = data['dest'].format(appid)
-    size = data.get('size')
-    filepath = os.path.join(output_dir, filename)
-
-    try:
-      response = session.get(url, headers = headers)
-      response.raise_for_status()
-    except Exception as ex:
-      logger.error(f'Could not download {url}:\n{ex}')
-      continue
-
-    save_asset(response.content, filepath, size)
-
-def save_asset(content, filepath, size = None):
-  try:
-    img = Image.open(BytesIO(content))
-    if size: img = img.resize(size, Image.LANCZOS)
-    img.save(filepath)
-    logger.success(f'Saved "{filepath}"')
-  except Exception as ex:
-    logger.error(f'Could not save {filepath}:\n{ex}')
 
 if __name__ == '__main__':
   try:
