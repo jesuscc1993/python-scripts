ce922fe6f9845b320ed9bcbedab7d8fcf96de917
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-07-09 11:37:07 +0200

    Add hardcoded settings

--- process_video/_common.py
+++ process_video/_common.py
@@ -1,6 +1,14 @@
 import re
 
 ENCODING = 'latin-1'
+FONT_ATTRIBUTES = ['color', 'face', 'size']
+
+STRIP_SETTINGS = {
+  'fonts': True,
+  'color': False,
+  'face': False,
+  'size': False,
+}
 
 def add_missing_spaces(file_path):
   with open(file_path, 'r', encoding = ENCODING, errors = 'replace') as f:
@@ -12,6 +20,14 @@ def add_missing_spaces(file_path):
 def strip_tags_from_subs_file(file_path):
   with open(file_path, 'r', encoding = ENCODING, errors = 'replace') as f:
     content = f.read()
-  content = re.sub(r'</?font\b[^>]*>', '', content, flags = re.IGNORECASE)
+  if STRIP_SETTINGS.get('fonts'):
+    content = re.sub(r'</?font\b[^>]*>', '', content, flags = re.IGNORECASE)
+  else:
+    for attr in FONT_ATTRIBUTES:
+      if STRIP_SETTINGS.get(attr):
+        content = strip_attribute(content, attr)
   with open(file_path, 'w', encoding = ENCODING) as f:
     f.write(content)
+
+def strip_attribute(content, attribute):
+  return re.sub(rf'\s*\b{attribute}=["\'][^"\']*["\']', '', content, flags = re.IGNORECASE)
