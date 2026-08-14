31848a967121df9cb6adba2013fd0171984f9135
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-08-14 18:04:15 +0200

    Add mklink_hidden script

---
+++ symlinks/mklink_hidden.py
@@ -0,0 +1,37 @@
+
+
+import ctypes
+import os
+
+from _common import make_link, run_as_admin
+
+from mtlogger import logger
+from mtprompt import Prompt
+
+FILE_ATTRIBUTE_HIDDEN = 0x02
+
+def main():
+  src_path = Prompt.path('Enter the path you want to create a hidden symlink for')
+  new_name = Prompt.str('Enter the new name for the item')
+
+  parent_dir = os.path.dirname(src_path)
+  dest_path = os.path.join(parent_dir, new_name)
+
+  os.rename(src_path, dest_path)
+  logger.success(f'Renamed "{src_path}" to "{dest_path}".')
+
+  is_dir = os.path.isdir(dest_path)
+  make_link(src_path, dest_path, target_is_directory=is_dir, make_dirs=False)
+  hide_path(src_path)
+
+def hide_path(path: str):
+  ctypes.windll.kernel32.SetFileAttributesW(path, FILE_ATTRIBUTE_HIDDEN)
+
+if __name__ == '__main__':
+  try:
+    run_as_admin()
+    main()
+  except Exception as ex:
+    logger.unhandledError(ex)
+
+  Prompt.enter_to_exit()
