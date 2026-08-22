19c4ba42f14fa5f3a427cd068a22ecd8869e865f
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-08-22 23:09:48 +0200

    Add compress_subfolders.py

---
+++ process_manga/compress_subfolders.py
@@ -0,0 +1,21 @@
+from mtlogger import logger
+from mtprompt import Prompt
+
+from _common import select_parent_folder, run_scripts_in_sequence
+
+SCRIPT_NAMES = [
+  '../compress/compress_subfolders',
+  'rename_items'
+]
+
+def process_parent_folder(
+  parent_folder_path: str,
+):
+  run_scripts_in_sequence(SCRIPT_NAMES, parent_folder_path)
+
+if __name__ == '__main__':
+  try:
+    select_parent_folder('Enter the path to the parent folder containing the folders you want to compress:\n', process_parent_folder)
+  except Exception as ex:
+    logger.unhandledError(ex)
+    Prompt.enter_to_exit()
