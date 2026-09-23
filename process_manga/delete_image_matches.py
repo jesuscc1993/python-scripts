5f47107ab08a7c64cc79052d7c478f1b8c5090f2
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-09-23 22:16:58 +0200

    Allow matchign several images at once.

--- process_manga/delete_image_matches.py
+++ process_manga/delete_image_matches.py
@@ -14,17 +14,17 @@ HASH_THRESHOLD = 10
 
 def main():
   if len(sys.argv) > 1:
-    folder = sys.argv[1]
+    folder_path = sys.argv[1]
   else:
-    folder = Prompt.dir(
+    folder_path = Prompt.dir(
       'Enter the path to the parent folder containing the chapter folders you want to clear of credits'
     )
 
   if len(sys.argv) > 2:
     img_path = sys.argv[2]
   else:
-    img_path = Prompt.file(
-      'Select the image file to match'
+    img_path = Prompt.path(
+      'Enter the path to the image file or folder containing the image files to match'
     )
 
   if len(sys.argv) > 3:
@@ -35,7 +35,7 @@ def main():
       default=1
     )
 
-  process_root(folder, img_path, img_spot)
+  process_root(folder_path, img_path, img_spot)
 
 def process_root(
   folder_path: str,
@@ -44,7 +44,15 @@ def process_root(
 ):
   logger.log(f'Deleting image matches for "{folder_path}"...')
 
-  ref_hash = imagehash.phash(Image.open(img_path))
+  if os.path.isdir(img_path):
+    ref_paths = [
+      os.path.join(img_path, f) for f in os.listdir(img_path)
+      if os.path.splitext(f)[1].lstrip('.').lower() in IMAGE_EXTENSIONS
+    ]
+  else:
+    ref_paths = [img_path]
+
+  ref_hashes = [imagehash.phash(Image.open(p)) for p in ref_paths]
   chapter_folders = sorted([
     os.path.join(folder_path, d) for d in os.listdir(folder_path)
     if os.path.isdir(os.path.join(folder_path, d))
@@ -52,8 +60,7 @@ def process_root(
 
   deleted = 0
   for chapter_folder in tqdm(chapter_folders):
-    if process_chapter(chapter_folder, ref_hash, img_spot):
-      deleted += 1
+    deleted += process_chapter(chapter_folder, ref_hashes, img_spot)
 
   if deleted == 0:
     logger.log('No image matches were found.')
@@ -62,18 +69,26 @@ def process_root(
 
 def process_chapter(
   chapter_folder: str,
-  ref_hash: imagehash.ImageHash,
+  ref_hashes: list[imagehash.ImageHash],
   img_spot: int,
 ):
-  last_img = get_image_by_spot(chapter_folder, img_spot)
-  if not last_img:
-    return
-  img_hash = imagehash.phash(Image.open(last_img))
-  if ref_hash - img_hash <= HASH_THRESHOLD:
-    send2trash(last_img)
-    tqdm.write(logger.format_info(f'Deleted "{last_img}"'))
-    return True
-  return False
+  ref_hashes = list(ref_hashes)
+  deleted = 0
+  while ref_hashes:
+    target_img = get_image_by_spot(chapter_folder, img_spot)
+    if not target_img:
+      break
+
+    img_hash = imagehash.phash(Image.open(target_img))
+    match = next((ref_hash for ref_hash in ref_hashes if ref_hash - img_hash <= HASH_THRESHOLD), None)
+    if not match:
+      break
+
+    send2trash(target_img)
+    tqdm.write(logger.format_info(f'Deleted "{target_img}"'))
+    ref_hashes.remove(match)
+    deleted += 1
+  return deleted
 
 def get_image_by_spot(
   folder_path: str,
